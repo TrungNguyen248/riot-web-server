4a3df19476866df65eca4b0074153754f7991333
TrungNguyen248 <nguyentrungnguyen12a3@gmail.com>
2023-04-21 00:27:57 +0700

    some change

--- controllers/news.js
+++ controllers/news.js
@@ -3,16 +3,16 @@ import { NewsModel } from "../models/NewsModel.js"
 export const getNews = async (req, res) => {
     try{
         let news = new NewsModel({
-            title: 'Looking Back on Women’s History Month 2023',
-            subTitle: "We celebrated Women’s History Month by hosting panels with women leaders, holding events to bring people together, and sharing women’s stories across Riot. ",
-            imageUrl: 'https://www.riotgames.com/darkroom/1000/0246c326644245311cf3b613c9d359b2:42ef0bf6eaa87379154e7d4725d2608a/2023-whm-recaparticle.png',
-            tag: 'inside riot',
-            urlNews: 'https://www.riotgames.com/en/news/womens-history-month-riot-rad-2023'
+            title: 'Building the Future of Sport at Riot Games',
+            subTitle: "John Needham, President of Esports at Riot Games, lays out his game plan toward building a sustainable future for the esports industry",
+            imageUrl: 'https://www.riotgames.com/darkroom/1000/592505fae56639af284572f290abbb6c:6cb1af3fcc39f7b7b837a024a6e441f0/rg-esports-johnneedham-one-shot-04-2023.png',
+            tag: 'one-shot',
+            urlNews: 'https://www.riotgames.com/en/news/building-the-future-of-sport-at-riot-games'
         })
         /* news.save() */
         const tag = req.query.q   
         if(tag) {
-            const newsByTag = await NewsModel.find({tag: tag})
+            const newsByTag = await NewsModel.find({tag: tag}).sort({createdAt : -1})
             res.status(200).json(newsByTag)
         }else {
             const news = await NewsModel.find().sort({createdAt : -1})
